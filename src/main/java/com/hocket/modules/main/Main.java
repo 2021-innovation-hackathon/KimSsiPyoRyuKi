79cc3a155410e5c6fe64ea70ad37c930fae05f43
kimtaejun <kim_taejun@naver.com>
2021-08-18 19:40:25 +0900

    heroky 배포 파일

--- src/main/java/com/hocket/modules/main/Main.java
+++ src/main/java/com/hocket/modules/main/Main.java
@@ -7,6 +7,7 @@ import com.hocket.modules.account.form.AccountForm;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @RequiredArgsConstructor
