57466385f299c50ccfe462c2102fc19f0736d013
kimtaejun <kim_taejun@naver.com>
2021-08-20 02:28:10 +0900

    properties값 환경변수로 변경

--- src/main/java/com/hocket/infra/config/S3Config.java
+++ src/main/java/com/hocket/infra/config/S3Config.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
 
-@PropertySource("classpath:application-dev.properties")
+@PropertySource("classpath:application-heroku.properties")
 @Configuration
 public class S3Config {
 
