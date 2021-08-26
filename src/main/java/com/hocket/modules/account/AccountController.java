c37d15a1258f5da1768336a2e7b80f63395ebf82
kimtaejun <kim_taejun@naver.com>
2021-08-27 01:43:25 +0900

    회원가입 잘못된 요청 로그 추가

--- src/main/java/com/hocket/modules/account/AccountController.java
+++ src/main/java/com/hocket/modules/account/AccountController.java
@@ -6,6 +6,7 @@ import com.hocket.modules.account.dto.AccountDto;
 import com.hocket.modules.kakao.KakaoService;
 import com.hocket.modules.kakao.dto.KakaoUserInfoResponseDto;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.springframework.cache.Cache;
 import org.springframework.cache.CacheManager;
@@ -14,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
-
+@Slf4j
 @RequiredArgsConstructor
 @RestController
 public class AccountController {
@@ -32,20 +33,27 @@ public class AccountController {
     public ResponseEntity signUp(String token) {
 
         kakaoService.checkToken(token);
-
         KakaoUserInfoResponseDto userInfo = kakaoService.getInfoByToken(token);
         if(userInfo.getEmail() == null){
+            log.info("null email");
+
             return ResponseEntity.badRequest().build();
 
         }
         if(userInfo.getNickname() == null){
+            log.info("null nickname");
+
             return ResponseEntity.badRequest().build();
 
         }
         if(userInfo.getAge_range() == null){
+            log.info("null age");
+
             return ResponseEntity.badRequest().build();
         }
         if(accountRepository.existsByEmail(userInfo.getEmail())){
+            log.info("exists email");
+
             return ResponseEntity.badRequest().build();
         }
         Account newAccount = accountService.saveAccount(userInfo);
